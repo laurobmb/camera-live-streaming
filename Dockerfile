7f65dcf9969f94221c42876851b0da8d3c0b72f0
Lauro Gomes <lagomes@redhat.com>
2022-05-20 16:31:21 -0300

    Ajustando projeto para 3 cameras

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,11 @@ COPY requirements.txt /
 RUN pip install pip --upgrade
 RUN pip install -r requirements.txt
 
-ENV LINK='rtsp://rtsp:password@IP:554/av_stream/ch0'
+# Intelbras IM5 e IM4 
+ENV LINK1='rtsp://admin:XXXXXXX@192.168.100.100:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif' 
+ENV LINK2='rtsp://admin:XXXXXXX@192.168.100.101:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif'
+# SONOFF (GK-200MP2-B)
+ENV LINK3='rtsp://rtsp:password@IP:554/av_stream/ch0'
 
 ADD ./templates/ /templates/
 COPY app.py /
--- app.py
+++ app.py
@@ -3,12 +3,16 @@ import cv2
 import os
 
 app = Flask(__name__)
-LINK = os.getenv('LINK')
-camera = cv2.VideoCapture(LINK)
+LINK1 = os.getenv('LINK1')
+LINK2 = os.getenv('LINK2')
+LINK3 = os.getenv('LINK3')
+camera1 = cv2.VideoCapture(LINK1)
+camera2 = cv2.VideoCapture(LINK2)
+camera3 = cv2.VideoCapture(LINK3)
 
-def gen_frames():
+def gen_frames(CAMERA):
     while True:
-        success, frame = camera.read()
+        success, frame = CAMERA.read()
         if not success:
             break
         else:
@@ -17,19 +21,36 @@ def gen_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen_frames(),
+@app.route('/video_feed1')
+def video_feed1():
+    return Response(gen_frames(camera1),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/video_feed2')
+def video_feed2():
+    return Response(gen_frames(camera2),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video_feed3')
+def video_feed3():
+    return Response(gen_frames(camera3),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/cam1')
+def index1():
+    return render_template('cam1.html')
+
+@app.route('/cam2')
+def index2():
+    return render_template('cam2.html')
+
+@app.route('/cam3')
+def index3():
+    return render_template('cam3.html')
 
 @app.route('/')
 def index():
-    """Video streaming home page."""
     return render_template('index.html')
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
